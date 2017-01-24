ae064b9baab6eef289c227d509a35899310c8d3b
angrifel <angrifel@users.noreply.github.com>
2017-01-24 14:34:03 -0300

    added versioning on artifacts

--- tools/package_on_release.cmd
+++ tools/package_on_release.cmd
@@ -2,8 +2,9 @@
 setlocal
 set CONFIGURATION=%1
 set BUILD_FOLDER=%2
+set VERSION=%3
 if  "%CONFIGURATION%"=="Release" (
-    7z a %BUILD_FOLDER%\mdlgen.zip %BUILD_FOLDER%\LICENSE %BUILD_FOLDER%\LICENSE-libyaml %BUILD_FOLDER%\LICENSE-YamlDotNet %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.exe %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.pdb
-    7z a %BUILD_FOLDER%\mdlgen_release.zip %BUILD_FOLDER%\LICENSE %BUILD_FOLDER%\LICENSE-libyaml %BUILD_FOLDER%\LICENSE-YamlDotNet %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.exe %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.pdb
+    7z a %BUILD_FOLDER%\mdlgen_%APPVEYOR_BUILD_VERSION%.zip %BUILD_FOLDER%\LICENSE %BUILD_FOLDER%\LICENSE-libyaml %BUILD_FOLDER%\LICENSE-YamlDotNet %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.exe %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.pdb
+    7z a %BUILD_FOLDER%\mdlgen_%APPVEYOR_BUILD_VERSION%_release.zip %BUILD_FOLDER%\LICENSE %BUILD_FOLDER%\LICENSE-libyaml %BUILD_FOLDER%\LICENSE-YamlDotNet %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.exe %BUILD_FOLDER%\src\ModelGenerator\bin\Release\mdlgen.pdb
 )
 endlocal
