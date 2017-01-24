043b2fe869c6bbe2034404b4c96502702202611e
angrifel <angrifel@users.noreply.github.com>
2017-01-24 14:56:38 -0300

    fixed keyword typo in on if statements

--- tools/push_artifacts.cmd
+++ tools/push_artifacts.cmd
@@ -6,15 +6,15 @@ set MDLGEN_PATH=%BUILD_FOLDER%\mdlgen_%VERSION%.zip
 set MDLGEN_RELEASE_PATH=%BUILD_FOLDER%\mdlgen_%VERSION%_release.zip
 set MDLGEN_DEBUG_PATH=%BUILD_FOLDER%\mdlgen_%VERSION%_debug.zip
 
-if exists "%MDLGEN_PATH%" (
+if exist "%MDLGEN_PATH%" (
     appveyor PushArtifact %MDLGEN_PATH% -name "mdlgen"
 )
 
-if exists "%MDLGEN_RELEASE_PATH%" (
+if exist "%MDLGEN_RELEASE_PATH%" (
     appveyor PushArtifact %MDLGEN_RELEASE_PATH% -name "mdlgen with symbols"
 )
 
-if exists "%MDLGEN_DEBUG_PATH%" (
+if exist "%MDLGEN_DEBUG_PATH%" (
     appveyor PushArtifact %MDLGEN_DEBUG_PATH% -name "mdlgen with symbols - debug"
 )
 endlocal
