872460e0fa90fcd6a341568216a2e0014e752e2e
angrifel <angrifel@users.noreply.github.com>
2017-02-01 20:41:31 -0300

    changed artifact names to use only dashes. added deployment section to appveyor.yml

--- tools/push_artifacts.cmd
+++ tools/push_artifacts.cmd
@@ -2,19 +2,19 @@
 setlocal
 set BUILD_FOLDER=%1
 set VERSION=%2
-set MDLGEN_PATH=%BUILD_FOLDER%\mdlgen_%VERSION%.zip
-set MDLGEN_RELEASE_PATH=%BUILD_FOLDER%\mdlgen_%VERSION%_release.zip
-set MDLGEN_DEBUG_PATH=%BUILD_FOLDER%\mdlgen_%VERSION%_debug.zip
+set MDLGEN_PATH=%BUILD_FOLDER%\mdlgen-%VERSION%.zip
+set MDLGEN_RELEASE_PATH=%BUILD_FOLDER%\mdlgen-%VERSION%-release-with-pdb.zip
+set MDLGEN_DEBUG_PATH=%BUILD_FOLDER%\mdlgen-%VERSION%-debug-with-pdb.zip
 
 if exist "%MDLGEN_PATH%" (
     appveyor PushArtifact %MDLGEN_PATH% -DeploymentName "mdlgen"
 )
 
 if exist "%MDLGEN_RELEASE_PATH%" (
-    appveyor PushArtifact %MDLGEN_RELEASE_PATH% -DeploymentName "mdlgen with symbols"
+    appveyor PushArtifact %MDLGEN_RELEASE_PATH% -DeploymentName "mdlgen release with pdb"
 )
 
 if exist "%MDLGEN_DEBUG_PATH%" (
-    appveyor PushArtifact %MDLGEN_DEBUG_PATH% -DeploymentName "mdlgen with symbols - debug"
+    appveyor PushArtifact %MDLGEN_DEBUG_PATH% -DeploymentName "mdlgen debug with pdb"
 )
 endlocal
