67bfe2d718c6f2aa0877f96a83f02db8c7418100
angrifel <angrifel@users.noreply.github.com>
2017-06-04 19:37:31 -0300

    fixed variable name on push_artifacts.cmd script

--- tools/push_artifacts.cmd
+++ tools/push_artifacts.cmd
@@ -7,14 +7,14 @@ set GEN_MDL_RELEASE_PATH=%BUILD_FOLDER%\gen-mdl-%VERSION%-release-with-pdb.zip
 set GEN_MDL_DEBUG_PATH=%BUILD_FOLDER%\gen-mdl-%VERSION%-debug-with-pdb.zip
 
 if exist "%GEN_MDL_PATH%" (
-    appveyor PushArtifact %GENMDL_PATH% -DeploymentName "gen-mdl release"
+    appveyor PushArtifact %GEN_MDL_PATH% -DeploymentName "gen-mdl release"
 )
 
 if exist "%GEN_MDL_RELEASE_PATH%" (
-    appveyor PushArtifact %GENMDL_RELEASE_PATH% -DeploymentName "gen-mdl release with pdb"
+    appveyor PushArtifact %GEN_MDL_RELEASE_PATH% -DeploymentName "gen-mdl release with pdb"
 )
 
 if exist "%GEN_MDL_DEBUG_PATH%" (
-    appveyor PushArtifact %GENMDL_DEBUG_PATH% -DeploymentName "gen-mdl debug with pdb"
+    appveyor PushArtifact %GEN_MDL_DEBUG_PATH% -DeploymentName "gen-mdl debug with pdb"
 )
 endlocal
